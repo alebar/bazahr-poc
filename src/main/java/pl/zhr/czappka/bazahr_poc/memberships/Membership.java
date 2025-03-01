07e2933b3e072be3f666a3651bbd799c5ddc73ba
alebar <>
2025-03-01 01:28:52 +0100

    switched to postgres

--- src/main/java/pl/zhr/czappka/bazahr_poc/memberships/Membership.java
+++ src/main/java/pl/zhr/czappka/bazahr_poc/memberships/Membership.java
@@ -3,11 +3,13 @@ package pl.zhr.czappka.bazahr_poc.memberships;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 import java.time.Instant;
 import java.util.UUID;
 
 @Entity
+@Table(name = "memberships")
 class Membership {
 
     @Id
--- src/test/java/pl/zhr/czappka/bazahr_poc/BazahrPocApplicationTests.java
+++ src/test/java/pl/zhr/czappka/bazahr_poc/BazahrPocApplicationTests.java
@@ -1,15 +1,33 @@
 package pl.zhr.czappka.bazahr_poc;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.context.annotation.Import;
 
+import pl.zhr.czappka.bazahr_poc.memberships.MembershipsCollectionDto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class BazahrPocApplicationTests {
+
+	@Autowired
+	TestRestTemplate testRestTemplate;
 
 	@Test
 	void contextLoads() {
+		assertThat(getMembershipsCount()).isEqualTo(0);
+
+		testRestTemplate.postForEntity("/v1/memberships/", null, null);
+
+		assertThat(getMembershipsCount()).isEqualTo(1);
+	}
+
+	private long getMembershipsCount() {
+		return testRestTemplate.getForEntity("/v1/memberships", MembershipsCollectionDto.class).getBody().getCount();
 	}
 
 }
